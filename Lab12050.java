2188acbcac898833fc82a85f59564cd7b0be666f
Cedric Wu Tchan Ki <ea891282@ens.uqam.ca>
2019-09-23 10:19:02 -0400

    Ajout du fichier java

---
+++ Lab12050.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1.pkg2050;
+
+/**
+ *
+ * @author ea891282
+ */
+public class Lab12050 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("FIZZBUZZ");
+            }
+           }
+        }
+    
+    
+}
